7b8207bf3e5e7e2f2d97d9a70d9ed2baeba12042
Hoang Minh <minh.ht173259@sis.hust.edu.vn>
2022-03-14 04:25:54 +0700

    first commit

---
+++ src/main/java/com/example/demo/User/UserApp.java
@@ -0,0 +1,14 @@
+package com.example.demo.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserApp {
+    private Long userId;
+    private String username;
+    private String password;
+}
